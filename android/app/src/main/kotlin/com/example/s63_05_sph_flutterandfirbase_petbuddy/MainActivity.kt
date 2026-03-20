7831f286944c5ad859906f0be294a988ddd27d85
Satvik Jain <satvikswm@gmail.com>
2026-03-20 13:56:12 +0530

    concept2 done

--- android/app/src/main/kotlin/com/example/s63_05_sph_flutterandfirbase_petbuddy/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/s63_05_sph_flutterandfirbase_petbuddy/MainActivity.kt
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 package com.company.petbuddy
-=======
-package com.example.s63_05_sph_flutterandfirbase_petbuddy
->>>>>>> 2e70ae1d06f444c0b3783788eb700947b65c305c
 
 import io.flutter.embedding.android.FlutterActivity
 
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,7 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-<<<<<<< HEAD
 #include <cloud_firestore/cloud_firestore_plugin_c_api.h>
 #include <firebase_auth/firebase_auth_plugin_c_api.h>
 #include <firebase_core/firebase_core_plugin_c_api.h>
@@ -18,8 +17,4 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("FirebaseAuthPluginCApi"));
   FirebaseCorePluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FirebaseCorePluginCApi"));
-=======
-
-void RegisterPlugins(flutter::PluginRegistry* registry) {
->>>>>>> 2e70ae1d06f444c0b3783788eb700947b65c305c
 }
